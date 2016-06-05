d28f32c091b9d36dbdab71d25d1ccda043a86cdf
Damjan Manevski <dame_mane@hotmail.com>
2016-06-05 22:04:26 +0200

    Osnovna vprasanja

--- Igra/src/igra/Okno.java
+++ Igra/src/igra/Okno.java
@@ -1,22 +1,62 @@
 package igra;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+
 
 
 @SuppressWarnings("serial")
-public class Okno extends JFrame {
+public class Okno extends JFrame implements MouseListener {
 	public Platno platno;
+	JButton zacni;
 	
 	public Okno(){
 		super();
-		setTitle("naslov");		
-		this.platno = new Platno(600, 600);
-		add(platno);
+		setTitle("naslov");
+		this.platno = new Platno(500, 400);
+		this.getContentPane().add(platno);
+		this.addMouseListener(this);
+		//this.setLayout(new GridBagLayout());
 		
+
 	}
 	public static void main(String[] args) {
 		Okno okno = new Okno();
 		okno.pack();
 		okno.setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+//		if(e.getSource()== zacni){
+//			repaint();
+//		}
+	}
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- Igra/src/igra/Platno.java
+++ Igra/src/igra/Platno.java
@@ -3,16 +3,79 @@ package igra;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
 public class Platno extends JPanel {
 	public int sirina;
 	public int visina;
+	private JTextField textField;
+	public String r;
+	public JTextField odgovor;
+	public String rr;
 	
 	public Platno(int sirina, int visina){
 		super();		
+
 		this.sirina = sirina;
 		this.visina = visina;
+
+		JButton zacni = new JButton("Za\u010Dni!");
+		zacni.setBounds(175, 197, 107, 23);
+		zacni.setForeground(Color.BLACK);
+		zacni.setBackground(Color.WHITE);
+		zacni.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				removeAll();
+				revalidate();
+				repaint();
+				JLabel vprasanje = new JLabel("Kolk je 2+3?");
+				vprasanje.setBounds(191, 118, 69, 14);
+				add(vprasanje);
+				odgovor = new JTextField();
+				odgovor.setBounds(103, 143, 239, 23);
+				add(odgovor);
+				JButton gumb = new JButton("Odgovori");
+				gumb.setBounds(175, 197, 107, 23);
+				add(gumb);
+				gumb.addActionListener(new ActionListener() {					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						rr = odgovor.getText();
+						if(rr.equals("5")){
+							r = "Tocno!";
+						}
+						else{
+							r = "Napacno!";
+						}
+						removeAll();
+						revalidate();
+						repaint();
+						JLabel kuku = new JLabel(r);
+						kuku.setBounds(191, 118, 69, 14);
+						add(kuku);
+
+					}
+				});
+
+			}
+		});
+		setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(103, 143, 239, 23);
+		add(textField);
+		textField.setColumns(10);
+		add(zacni);
+		
+		JLabel VnesiIme = new JLabel(" Vnesi ime:");
+		VnesiIme.setBounds(191, 118, 69, 14);
+		add(VnesiIme);
 	
 	}
 	
@@ -24,5 +87,4 @@ public class Platno extends JPanel {
 		// TODO Auto-generated method stub
 
 	}
-
 }
